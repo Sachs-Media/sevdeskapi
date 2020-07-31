b534d0f17901abdb6f2de9282f59b8e5cfb0917b
Stefan Eiermann <git@ultraapp.de>
2020-07-31 13:09:05 +0200

    Add Bump2version; add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sevdeskapi",
+    version="0.0.1",
+    author="Stefan Eiermann, Sachs Media",
+    author_email="support@sachs-media.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sachs-Media/sevdeskapi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
